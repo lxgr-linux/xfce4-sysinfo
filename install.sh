219a769e1e266717bdeb93caeb1ecf7f1119be1e
lxgr <lxgr@protonmail.com>
2020-05-08 14:11:02 +0200

    Host

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 cp ./sysinfo /usr/bin/
 cp ./xfce4-sysinfo.desktop /usr/share/applications/
-cp ./xfce4-sysinfo.conf /etc
+cp ./xfce4-sysinfo.conf /etc/
 mkdir /usr/share/xfce4-sysinfo/
 cp -r ./icons/* /usr/share/pixmaps/
 touch /usr/share/xfce4-sysinfo/version
