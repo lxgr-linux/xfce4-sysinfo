3116d1a7bfbcf6e1d2ea82a8c8154a9c71157893
lxgr <lxgr@protonmail.com>
2020-05-08 13:45:21 +0200

    conf file

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@
 
 cp ./sysinfo /usr/bin/
 cp ./xfce4-sysinfo.desktop /usr/share/applications/
+cp ./xfce4-sysinfo.conf /etc
 mkdir /usr/share/xfce4-sysinfo/
 cp -r ./icons/* /usr/share/pixmaps/
 touch /usr/share/xfce4-sysinfo/version
